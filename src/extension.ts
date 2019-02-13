e87d65688158194f20476e31e12796b4d0e70839
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2019-02-13 20:24:52 +0100

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import get from 'axios';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'extension.showChangelog',
     async () => {
@@ -17,11 +10,13 @@ export function activate(context: vscode.ExtensionContext) {
           vscode.workspace.workspaceFolders[0].uri.path + '/package.json'
         );
 
-        const parsed = JSON.parse(packageJson.getText());
+        const { dependencies, devDependencies } = JSON.parse(
+          packageJson.getText()
+        );
 
         const allPackages = Object.keys({
-          ...parsed.dependencies,
-          ...parsed.devDependencies
+          ...dependencies,
+          ...devDependencies
         });
 
         const chosenPackage = await vscode.window.showQuickPick(allPackages);
@@ -77,5 +72,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
